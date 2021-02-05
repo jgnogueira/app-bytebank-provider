6ac0f5ce3b670f0763b331cbd79fdecbc0af1591
jgnogueira <josegmnogueira@hotmail.com>
2021-02-05 05:00:43 -0300

    Criando Dashboard para tela inicial

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bytebank'),
+      ),
+      body: Text('40.0'),
+    );
+  }
+}

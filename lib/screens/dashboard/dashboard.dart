b05c0f7384e41aab9e8e14b72993b1eeadf17886
jgnogueira <josegmnogueira@hotmail.com>
2021-02-09 04:36:44 -0300

    Criando botão para adicionar valor no saldo

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,5 +1,7 @@
+import 'package:bytebank/models/saldo.dart';
 import 'package:bytebank/screens/dashboard/saldo.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -7,10 +9,22 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Bytebank'),
       ),
-      body: Align(
-        child: SaldoCard(),
-        alignment: Alignment.topCenter,
-      ),
+      body: ListView(children: <Widget>[
+        Align(
+          child: SaldoCard(),
+          alignment: Alignment.topCenter,
+        ),
+        Consumer<Saldo>(
+          builder: (context, saldo, child) {
+            return RaisedButton(
+              onPressed: () {
+                saldo.adicionar(10.0);
+              },
+              child: Text('Adiciona'),
+            );
+          },
+        ),
+      ]),
     );
   }
 }

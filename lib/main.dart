dfcea08e7cff80a0b334f31ac55cd294204216df
jgnogueira <josegmnogueira@hotmail.com>
2021-02-10 04:20:15 -0300

    Alterando método Provider para gerenciar o app

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(
-      ChangeNotifierProvider(
-        create: (context) => Saldo(0),
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => Saldo(0),
+          ),
+        ],
         child: BytebankApp(),
       ),
     );

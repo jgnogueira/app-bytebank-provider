09ad75c7f1466006c995fdd1bdedc055928dc39b
jgnogueira <josegmnogueira@hotmail.com>
2021-02-10 04:27:26 -0300

    Adicionando Provider para gerenciar transferências

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bytebank/models/saldo.dart';
+import 'package:bytebank/models/transferencias.dart';
 import 'package:bytebank/screens/dashboard/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,6 +9,9 @@ void main() => runApp(
         providers: [
           ChangeNotifierProvider(
             create: (context) => Saldo(0),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => Transferencias(),
           ),
         ],
         child: BytebankApp(),
